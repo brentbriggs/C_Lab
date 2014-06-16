94e61fe1fc8d5887540a0c2679807dee1f945584
Brent Briggs <brent.briggs@gmail.com>
2014-06-16 07:16:35 -0400

    Added main() for testing.

--- src/file_io.c
+++ src/file_io.c
@@ -7,7 +7,7 @@
  * www.wtfpl.net. So do WTF you want.
  *
  * DESCRIPTION:
- * Implementation for text_file_s structure manipulation functions.
+ * Implementation for file_io functions.
  *****************************************************************************/
 #include "file_io.h"
 
@@ -25,7 +25,7 @@ int file_clear(char *filename) {
 /** Wrapper for fclose() from stdio.h. */
 int file_close(FILE *file) {
     if (fclose(file) == EOF ) {
-        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+        fprintf(stderr, "ERROR Closing File: %s\n", strerror(errno));
         return 1;
     }
     return 0;
@@ -34,7 +34,7 @@ int file_close(FILE *file) {
 /** Compare text files line by line. Return the line number where they differ, 0
  * if they are the same, -1 for error. */
 int file_compare(char *filename1, char *filename2) {
-    bool diff = false;
+    int diff = 0;
 
     FILE *file1 = file_open(filename1, MODE_READ_BEGINNING);
     FILE *file2 = file_open(filename2, MODE_READ_BEGINNING);
@@ -51,19 +51,19 @@ int file_compare(char *filename1, char *filename2) {
             (fgets(file2_line, MAX_LINE_LENGTH, file2))) {
         ++line_count;
         if(strcmp(file1_line, file2_line) != 0 ) {
-            diff = true;
+            diff = 1;
             break;
         }
     }
     if((fgets(file1_line, MAX_LINE_LENGTH, file1)) ||
             (fgets(file2_line, MAX_LINE_LENGTH, file2))) {
-        diff = true;
+        diff = 1;
         ++line_count;
     }
 
     file_close(file1);
     file_close(file2);
-    if(diff)
+    if(diff == 1)
         return line_count;
     else
         return 0;
@@ -75,7 +75,7 @@ int file_copy(char *filename1, char *filename2) {
     int status = 0;
 
     FILE *file1 = file_open(filename1, MODE_READ_BEGINNING);
-    FILE *file2 = file_open(filename2, MODE_WRITE_TRUNCATE);
+    FILE *file2 = file_open(filename2, MODE_READ_WRITE_TRUNCATE);
     if((file1 == NULL) || (file2 == NULL)) {
         fprintf(stderr, "ERROR: Could not copy file1 %s to %s.",
                 filename1, filename2);
@@ -85,7 +85,7 @@ int file_copy(char *filename1, char *filename2) {
         while((c = fgetc(file1)) != EOF) {
             fputc(c, file2);
         }
-        if(!(file_close(file1) == file_close(file2) == 0)) {
+        if(!(file_close(file1) == 0) || !(file_close(file2) == 0)) {
             fprintf(stderr,
                     "ERROR: Could not copy file1 %s to %s.",
                     filename1, filename1);
@@ -93,8 +93,6 @@ int file_copy(char *filename1, char *filename2) {
         }
     }
 
-    file_close(file1);
-    file_close(file2);
     return status;
 }
 
@@ -157,7 +155,9 @@ int file_line_count(char *filename) {
     char line[MAX_LINE_LENGTH];
 
     while(fgets(line, MAX_LINE_LENGTH, file)) {
+        if (line[0] != EOF) {
             line_count++;
+        }
     }
 
     file_close(file);
@@ -198,21 +198,68 @@ int file_put_line(char* filename, char *string) {
         return 1;
     }
 
-    if (fputs(string, file) && fputs("\n", file)) {
+    if (fputs(string, file)) {
         fflush(file);
         file_close(file);
         return 0;
     }
     else
+    {
         file_close(file);
         return 1;
+    }
 }
 
-int main(int argc, char *argv[])
+int main()
 {
     char *filename1 = "test_file1.txt";
     char *filename2 = "test_file2.txt";
     file_clear(filename1);
     file_clear(filename2);
+
+    /* Put a line in the files and test the count */
+    assert(file_put_line(filename1,
+                         "This is a test line for test_file1.txt.\n")
+           != 1);
+   assert(file_put_line(filename2,
+                         "This is a test line for test_file2.txt.\n")
+           != 1);
+    assert(file_line_count(filename1) == 1);
+    assert(file_line_count(filename2) == 1);
+
+    /* Put a second line in the files and test that the count increases. */
+    assert(file_put_line(filename1,
+                         "This is a test line for test_file1.txt.\n")
+           != 1);
+   assert(file_put_line(filename2,
+                         "This is a test line for test_file2.txt.\n")
+           != 1);
+    assert(file_line_count(filename1) == 2);
+    assert(file_line_count(filename2) == 2);
+
+    /* Print the contents of both files */
+    printf("%s Contents:\n", filename1);
+    assert(file_print(filename1) == 0);
+    printf("\n");
+    printf("%s Contents:\n", filename2);
+    assert(file_print(filename2) == 0);
+    printf("\n");
+
+    /* Compare both files and verify that they are different. */
+    assert(file_compare(filename1, filename2) != -1);
+    assert(file_compare(filename1, filename2) > 0);
+
+    /* Copy contents of file 1 to file 2 and verify that they are the same */
+    assert(file_copy(filename1, filename2) == 0);
+    assert(file_compare(filename1, filename2) == 0);
+
+    /* Clear both files out and test that the count is zero. */
+    assert(file_clear(filename1) == 0);
+    assert(file_clear(filename2) == 0);
+    assert(file_line_count(filename1) == 0);
+    assert(file_line_count(filename2) == 0);
+
+
+
     return 0;
 }
--- src/file_io.h
+++ src/file_io.h
@@ -9,9 +9,9 @@
  * DESCRIPTION:
  * Definition for "file" object and functions.
  *****************************************************************************/
+#include <assert.h>
 #include <errno.h>
 #include <stdarg.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
